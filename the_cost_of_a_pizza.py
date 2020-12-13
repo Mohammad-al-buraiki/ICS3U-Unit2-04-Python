31641860975e4f87476a8af1f2808eab7843b1fb
Ubuntu <ubuntu@ip-172-31-5-27.ec2.internal>
2020-12-13 05:10:01 +0000

    the_cost_of_a_pizza.py

--- the_cost_of_a_pizza.py
+++ the_cost_of_a_pizza.py
@@ -6,21 +6,24 @@
 
 import constants
 
+
 def main():
     # this function calculates the  cost of a pizza
 
     # input
     diameter = input("Enter the diameter of the pizza (inch) :")
-    
+
     # process
-    cost1 = (int(diameter)*(constants.MATERIALS)+(constants.LABOR)+(constants.RENT))
+    cost1 = (int(diameter)*(constants.MATERIALS)+(constants.LABOR)
+             + (constants.RENT))
     cost2 = (cost1)*(constants.HST)
     total = (cost1)+(cost2)
-    
+
     # output
     print("")
-    print("The cost for a {0} inch pizza is: ${1:,.2f}".format(diameter, total))
-    
-    
+    print("The cost for a {0} inch pizza is: ${1:,.2f}"
+          .format(diameter, total))
+
+
 if __name__ == "__main__":
     main()
